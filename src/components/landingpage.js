c96be347b4af2043439e43d6cdeb3370429cfa77
Mburu <john.mburu@wirecard.com>
2020-05-23 16:49:19 +0200

    Added predict button on the landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,25 +1,32 @@
 import React, {Component} from 'react';
-import { Form } from 'react-bootstrap' ;
+import { Form, Button } from 'react-bootstrap' ;
 
 class Upload extends Component{
     render(){
         return(
 
             <div>
-                <div className="center-upload">
-                    <Form>
-                    <Form.File 
-                        id="custom-file"
-                        label="Upload Dataset"
-                        custom
-                        accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" 
-                    />
-                    </Form>
+                <Form>
+                    <div className="center-upload">
                     
-                    <div className="upload-text">
-                        Upload will only accept <strong>.xls</strong>, <strong>.xlsx </strong>and <strong>.csv</strong> files
+                        <Form.File 
+                            id="custom-file"
+                            label="Upload Dataset"
+                            custom
+                            accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" 
+                        />
+                        
+                        
+                        <div className="upload-text">
+                            Upload will only accept <strong>.xls</strong>, <strong>.xlsx </strong>and <strong>.csv</strong> files
+                        </div>
+
+                        <div className="center-upload-button">
+                        <Button variant="primary">Analyse and Predict</Button>{' '}
+                        </div>
+                  
                     </div>
-                </div>
+                </Form> 
 
             </div>
         )
